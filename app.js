5534331b9b554ad0eb9bfd805c684187489bf45f
Belinda1704 <b.larose@alustudent.com>
2024-03-29 12:51:07 +0200

    All completed

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 const taskForm = document.getElementById("task-form");
 const taskList = document.getElementById("task-list");
 
-// console.log(taskForm, taskList)
+// console.log(taskForm, taskList);
+
+taskForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const taskInput =document.getElementById("task-input");
+    const taskText = taskInput.Value.trim();
+
+    // console.log(taskText);
+
+    if(taskText !==""){
+        // Create a new task item
+
+        const taskItem = document.createElement("li");
+        taskItem.classList.add("task-item");
+        taskItem.textContent = taskText;
+
+        taskItem.addEventListener("click", function(){
+            console.log("completed");
+            this.classList.toggle("completed");
+        });
+
+        taskList.appendChild(taskItem);
+
+        taskInput.Value = "";
+    }
+});
